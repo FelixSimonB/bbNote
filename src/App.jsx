859144970ecaf9c3b3ddf2cac2c1214e2d892d4e
FelixSimonB <barredo.simon@gmail.com>
2025-03-18 23:16:46 +0800

    fix font

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return (
       <div id="app">
             <Title />
-            <img src="https://64.media.tumblr.com/26fd871878ac34750c7b228e5bf69cf0/0cc3599b3ded92c1-11/s250x400/0ee72523398ad39499bd823d808b2f2c8b84fc0e.gif" class="sticker-2"></img>
+            <img src="https://64.media.tumblr.com/26fd871878ac34750c7b228e5bf69cf0/0cc3599b3ded92c1-11/s250x400/0ee72523398ad39499bd823d808b2f2c8b84fc0e.gif" className="sticker-2"></img>
             <NotesProvider>
                 <NotesPage />
             </NotesProvider>
